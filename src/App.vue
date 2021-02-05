322b99472d9d84cc120b12a0feb91351378704d8
maximshadchin <70339016+maximshadchin@users.noreply.github.com>
2021-02-05 23:26:46 +0300

    v0

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <v-table/>
+    <p>Hello</p>
+  </div>
+</template>
+
+<script>
+
+import vTable from './components/v-table'
+
+export default {
+  name: 'vTable',
+  components: {
+    vTable
+  }
+}
+
+
+
---
+++ src/components/v-table.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="v-table">
+    <h1></h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name : 'v-table',
+  components: {},
+  methods: {},
+  computed: {}
+}
+</script>
